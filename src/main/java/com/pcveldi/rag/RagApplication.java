0692732066e70b9ec570d3141580182e77e03212
Pc Veldi <65594452+PcVeldi22@users.noreply.github.com>
2026-07-06 12:14:53 -0400

    Create RagApplication class for Spring Boot application

---
+++ src/main/java/com/pcveldi/rag/RagApplication.java
@@ -0,0 +1,14 @@
+package com.pcveldi.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class RagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagApplication.class, args);
+    }
+}
